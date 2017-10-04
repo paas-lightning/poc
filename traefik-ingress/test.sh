685a0a670b523ed0c4301171c4e2a3aa140f8740
Cloud User <centos@load-balancer.atom.external>
2017-10-04 22:55:25 +0200

    Namespace correction

--- traefik-ingress/test.sh
+++ traefik-ingress/test.sh
@@ -9,11 +9,11 @@ export API_PROTOCOL="`kubectl get endpoints kubernetes -o json | jq '.subsets[].
 export API_PORT="`kubectl get endpoints kubernetes -o json | jq '.subsets[].ports[].port' | tr -d '"' `"
 export API_HOST="`kubectl get endpoints kubernetes -o json | jq '.subsets[].addresses[].ip' | tr -d '"' `"
 
-
-kubectl get pods -n poc
+NAMESPACE="poc"
+kubectl get pods -n $NAMESPACE
 kubectl get pods -n poc-client
 kubectl get replicaset -n poc-client
-kubectl get service -n poc
+kubectl get service -n $NAMESPACE
 kubectl get service -n poc-client
 
 curl -k --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" "$API_PROTOCOL://$API_HOST:$API_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses"
